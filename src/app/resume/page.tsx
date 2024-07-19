92323322370e7af6d62388c2c865b45af0a70e45
sonnh <hoaisonkt1998@gmail.com>
2024-07-19 10:34:26 +0700

    Implement code resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,8 +2,119 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>This is resume page</h1>
+    <div className="h-auto w-full flex md:flex-col flex-col justify-around items-center">
+      <div className="p-5">
+        <span className=" text-black font-bold 2xl:text-xl xl:text-lg lg:text-base md:text-sm smx:text-xs">
+          RESUME
+        </span>
+      </div>
+      {/* Education */}
+      <div className=" w-full flex md:flex-row flex-col justify-around items-start p-3 border-b border-gray-400">
+        <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex items-center justify-center text-center">
+          <span className=" text-black font-bold 2xl:text-xl xl:text-lg lg:text-base md:text-sm smx:text-xs">
+            Education
+          </span>
+        </div>
+        <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex md:flex-col flex-col justify-start items-start">
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                09/2016 -- 05/2018
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                University Of Science And Technology
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I study at University Of Science And Technology. My major is
+                construction engineer.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                05/2018 -- 12/2020
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                FPT University
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I graduated from FPT university with Bachelor of IT degree.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Work Experience */}
+      <div className=" w-full flex md:flex-row flex-col justify-around items-start p-3 border-b border-gray-400">
+        <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex items-center justify-center text-center">
+          <span className=" text-black font-bold 2xl:text-xl xl:text-lg lg:text-base md:text-sm smx:text-xs">
+            Education
+          </span>
+        </div>
+        <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex md:flex-col flex-col justify-start items-start">
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                06/2018 -- 09/2018
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                WEB DEVELOP, FPT EDUCATION
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I used to development nguyenduc.edu.vn website, DaNangTravel
+                website, ShoeShop website,... I use skill list HTML5 - CSS3 -
+                Jquery - Boostrap - photoshop,...
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                10/2018 -- 11/2019
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                MOBILE DEVELOP, FPT EDUCATION
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I used to development Quan Ly Thu Chi App, Book Manager App,
+                Vocabulary App, Tro Choi Toan Hoc App, May Tinh App, Bao Thuc
+                App, Lich App, Quan Ly Benh Xa App,... I use skill list Java,
+                Android, Photoshop.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                01/2020 -- 03/2020
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                JOIN STOCK COMPANY V.B.P.O, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I work development website with Python Django. I’m a fresher
+                position. I use skill list JHTML5 - CSS3 - Jquery - Python,...
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Skills & Expertise */}
     </div>
   );
 };
