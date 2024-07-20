a6a28cdca9e9fd494511df260a4d7ac35c734d83
sonnh <hoaisonkt1998@gmail.com>
2024-07-20 15:30:44 +0700

    Update resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -28,7 +28,8 @@ const page = () => {
               </span>
               <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
                 I study at University Of Science And Technology. My major is
-                construction engineer.
+                construction engineer. With a passion for information
+                technology. So I decided to change my major.
               </span>
             </div>
           </div>
@@ -51,10 +52,10 @@ const page = () => {
         </div>
       </div>
       {/* Work Experience */}
-      <div className=" w-full flex md:flex-row flex-col justify-around items-start p-3 border-b border-gray-400">
+      <div className=" w-full flex md:flex-row flex-col justify-around items-start p-3 border-b border-gray-400 pt-10">
         <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex items-center justify-center text-center">
           <span className=" text-black font-bold 2xl:text-xl xl:text-lg lg:text-base md:text-sm smx:text-xs">
-            Education
+            Work Experience
           </span>
         </div>
         <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex md:flex-col flex-col justify-start items-start">
@@ -106,8 +107,145 @@ const page = () => {
                 JOIN STOCK COMPANY V.B.P.O, DA NANG
               </span>
               <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
-                I work development website with Python Django. I’m a fresher
-                position. I use skill list JHTML5 - CSS3 - Jquery - Python,...
+                I work website with Python Django. I’m a fresher position. I use
+                skill list HTML5 - CSS3 - Jquery - Python Django - Docker,...
+                After two months of probation, I was officially hired by the
+                company. I cannot go to school and work at the company at the
+                same time. So I chose to continue studying to graduate.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                11/2020 -- 04/2021
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                TECHNOLOGY COMPANY TMT, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I work Crawl data with Python - Trainning Machine Learning. I’m
+                a developer position. I use skill list HTML5 - CSS3 - Jquery -
+                Selenium - Python Django - MongoDB - Docker,...I use python and
+                selenium to crawl newspaper pages (https://www.24h.com.vn/,
+                https://tuoitre.vn/, https://vnexpress.net/,...), crawl
+                e-commerce sites of Viet Nam (https://shopee.vn/,
+                https://www.sendo.vn/, https://www.lazada.vn/,...), crawl
+                e-commerce sites of Trung Quoc (https://world.taobao.com/,
+                https://www.alibaba.com/,...). Then I analyze the data and I use
+                MongoDB to save data.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                04/2021 -- 07/2021
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                MTI TECHNOLOGY, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I self-taught C# language. I work Talent Board project. I’m a
+                developer position. I use skill list Vue.js - C# - SQL
+                server...I work in groups. We work according to an agile
+                process. Back-end with C#, Front-end with Vue.js and MongoDB
+                database. We work on employee systems. It displays the skill and
+                level of each employee. In project, I work Back-end and
+                Front-end. I implement task and demo with Product Owner.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                08/2021 -- 03/2022
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                MTI TECHNOLOGY, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I self-taught Java language. I work Medica project. I’m a
+                developer position. I use skill list Vue.js - Java - SQL
+                server...I work in groups. We work according to an agile
+                process. Back-end with Java, Front-end with Vue.js and MongoDB
+                database. We work on the hospital’s drug management system. In
+                project, I work Back-end and Front-end. I implement task and
+                demo with Product Owner.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                04/2022 -- 08/2022
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                MTI TECHNOLOGY, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I work migration source code from Bitbucket to GitHub for ONE
+                Company.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                09/2022 -- 11/2022
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                MTI TECHNOLOGY, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I self-taught ReactJS language and NodeJS language. I self-study
+                on w3school. I self-study NestJS on https://nestjs.com/. Then I
+                work project demo. I self-study NextJS on https://nextjs.org/.
+                Then I work project demo.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                12/2022 -- Present
+              </span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                MTI TECHNOLOGY, DA NANG
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I work PLTool project. I’m a developer position. I use skill
+                list React - NodeJS - NextJs - NestJs - MongoDB,... I work in
+                groups. We work according to an agile process. Back-end with
+                NestJS, Front-end with NextJS and MongoDB database. We work on
+                employee management systems. The system is used by human
+                resources and management departments. For the human resources
+                department, they can add, edit, and delete one or more employees
+                by importing excel files. The system automatically calculates
+                salaries and exports excel files. With the management
+                department, they can add new projects, add people to the
+                project, and display project information. The system has charts
+                for statistics on employees, projects and cash flow. In project,
+                I work Back-end and Front-end. I implement task and demo with
+                Product Owner.
               </span>
             </div>
           </div>
@@ -115,6 +253,44 @@ const page = () => {
       </div>
 
       {/* Skills & Expertise */}
+      <div className=" w-full flex md:flex-row flex-col justify-around items-start p-3 border-b border-gray-400">
+        <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex items-center justify-center text-center">
+          <span className=" text-black font-bold 2xl:text-xl xl:text-lg lg:text-base md:text-sm smx:text-xs">
+            Skills & Expertise
+          </span>
+        </div>
+        <div className=" 2xl:w-1/2 xl:w-1/2 md:w-auto flex md:flex-col flex-col justify-start items-start">
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs"></span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                {/* University Of Science And Technology */}
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I have a list of skills C#, ReactJS, NodeJS, NestJS, NextJS,
+                Vue.js, SQL Server, Java, HTML5 & Css, Taiwindcss, JavaScript,
+                Android, Flutter.
+              </span>
+            </div>
+          </div>
+
+          <div className=" md:w-full flex md:flex-row flex-col justify-start items-start pb-10">
+            <div className=" 2xl:w-1/4 xl:w-1/4 lg:w-1/4 md:w-full sm:w-full smx:w-full">
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs"></span>
+            </div>
+            <div className=" 2xl:w-3/4 smx:w-auto flex md:flex-col flex-col justify-around items-start">
+              <span className="font-bold 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                {/* University Of Science And Technology */}
+              </span>
+              <span className="2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-sm smx:text-xs">
+                I can communicate with customers in English.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
